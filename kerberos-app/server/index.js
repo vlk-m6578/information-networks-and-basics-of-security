ddf26a134977b10c2c9b2e79b3d5e9e96613aca2
Milana L <lebedeva310@icloud.com>
2026-02-11 00:24:09 +0300

    as-server

--- kerberos-app/server/index.js
+++ kerberos-app/server/index.js
@@ -1,18 +1,73 @@
 const express = require('express');
-// const WebSocket = require('ws');
 const path = require('path');
+const as = require('./kerberos/as')
+// const WebSocket = require('ws');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.json());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 // const wss = new WebSocket.Server({port: 8080});
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/', (request, respond) => {
-  respond.send('Kerberos server is working')
+  respond.send('Server server is working')
 })
+
+app.post('/api/authenticate', (request, respond) => {
+  try {
+    const {username, password} = request.body;
+
+    console.log(`Request to authentification from ${username}`);
+
+    const result = as.authenticate(username, password);
+
+    respond.json({
+      success: true,
+      message: 'Authentification success',
+      data: result
+    })
+  } catch (error) {
+    respond.status(401).json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+app.post('/api/validate-tgt', (request, respond) => {
+  try {
+    const {tgtId } = request.body;
+
+    const result = as.validateTGT(tgtId);
+
+    respond.json({
+      success: result.valid,
+      message: result.valid ? 'TGT valid' : result.reason,
+      data: result
+    });
+
+  } catch (error) {
+    respond.status(401).json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+
+app.get('/api/debug/tickets', (req, res) => {
+  res.json({
+    tickets: as.issuedTickets,
+    count: Object.keys(as.issuedTickets).length
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running: ${PORT}`);
---
+++ kerberos-app/server/kerberos/as.js
@@ -0,0 +1,116 @@
+const crypto = require('crypto');
+
+class AuthenticationServer {
+  constructor() {
+    this.secretKey = crypto.randomBytes(32);
+
+    this.algorithm = 'aes-256-cbc';
+
+    this.issuedTickets = {};
+    // { id tgt: user info }
+  }
+
+  authenticate(username, password) {
+    //db
+    const validUsers = {
+      'alice': 'password123',
+      'bob': 'qwerty123'
+    };
+
+    if (!validUsers[username] || validUsers[username] !== password) throw new Error('Invalid credentials');
+
+    const tgt = this.generateTGT(username);
+    const sessionKey = this.generateSessionKey();
+
+    this.issuedTickets[tgt.id] = {
+      username: username,
+      issuedAt: new Date(),
+      sessionKey: sessionKey
+    };
+
+    console.log(`AS-server: TGT for ${username}`);
+
+    return {
+      tgt: tgt,
+      sessionKey: sessionKey
+    }
+  }
+
+  generateTGT(username) {
+    const tgtData = {
+      id: crypto.randomBytes(16).toString('hex'),
+      username: username,
+      issuedAt: new Date().toISOString(),
+      expiresAt: new Date(Date.now() + 3600000).toISOString(),
+    };
+
+    let encryptedTGT = this.encrypt(JSON.stringify(tgtData));
+    
+    return {
+      encrypted: encryptedTGT,
+      id: tgtData.id
+    };
+  }
+
+  generateSessionKey() {
+    return crypto.randomBytes(32).toString('hex');
+  }
+
+  encrypt(text) {
+    try {
+      const iv = crypto.randomBytes(16);
+
+      const cipher = crypto.createCipheriv(this.algorithm, this.secretKey, iv);
+
+      let encrypted = cipher.update(text, 'utf8', 'hex');
+      encrypted += cipher.final('hex');
+
+      return iv.toString('hex') + ':' + encrypted;
+      
+    } catch (error) {
+      console.error('Encrypt error:', error);
+      throw new Error('Encryption failed');
+    }
+  }
+
+  decrypt(encryptedText) {
+    try {
+      const parts = encryptedText.split(':');
+      const iv = Buffer.from(parts[0], 'hex');
+      const encrypted = parts[1];
+
+      const decipher = crypto.createDecipheriv(this.algorithm, this.secretKey, iv);
+
+      let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+      decrypted += decipher.final('utf8');
+
+      return decrypted;
+
+    } catch (error) {
+      console.error('Decrypt error:', error);
+      throw new Error('Decryption failed');
+    }
+  }
+
+  validateTGT(tgtId) {
+    const ticket = this.issuedTickets[tgtId];
+
+    if (!ticket) return { valid: false, reason: 'TGT not found' };
+
+    const issuedTime = new Date(ticket.issuedAt);
+    const currentTime = new Date();
+    const hoursDiff = (currentTime - issuedTime) / (60 * 1000);
+
+    if (hoursDiff > 60) return { valid: false, reason: 'TGT expired' };
+
+    return {
+      valid: true,
+      username: ticket.username,
+      sessionKey: ticket.sessionKey
+    }
+  }
+}
+
+const as = new AuthenticationServer();
+
+module.exports = as;
